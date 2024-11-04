990110b73d1c37a8310fd82c406da4b0ed298b87
Fabio Henrique <fabiosilva5210@gmail.com>
2024-11-03 23:20:19 -0300

    add consultas com PHP em index, mc e batalhas

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php require_once("paistropical.php") ?>
 
 <!-- Script para listar as batalhas -->
-<?php 
-    //Query para as consultas das batalhas (Trocar pela query correta)
-    $listarBatalhas = "SELECT * FROM eventos LIMIT 3";
+<?php
+//Query para as consultas das batalhas (Trocar pela query correta)
+$listarBatalhas = "SELECT * FROM eventos LIMIT 3";
 
-    $batalhas = mysqli_query($conecta_versovivo, $listarBatalhas);
+$batalhas = mysqli_query($conecta_versovivo, $listarBatalhas);
 
-    //Array com as informações das caixas de batalhas
-    $caixaBatalhas = mysqli_fetch_assoc($batalhas);
+//Array com as informações das caixas de batalhas
+$caixaBatalhas = mysqli_fetch_assoc($batalhas);
 
-    //Formatação da data e hora
-    $dataHoraFormatada = date("d/m H:i", strtotime($caixaBatalhas["data_hora_evento"]));
+//Formatação da data e hora
+$dataHoraFormatada = date("d/m H:i", strtotime($caixaBatalhas["data_hora_evento"]));
 ?>
 
 <!-- Script para listar os MCs -->
- <?php
-    $listarMcs = "SELECT * FROM usuarios WHERE status_conta = 1";
+<?php
+$listarMcs = "SELECT * FROM usuarios WHERE status_conta = 1 LIMIT 6";
 
-    $mcs = mysqli_query($conecta_versovivo, $listarMcs);
+$mcs = mysqli_query($conecta_versovivo, $listarMcs);
 
-    $caixasMcs = mysqli_fetch_assoc($mcs);
- ?>
+$caixasMcs = mysqli_fetch_assoc($mcs);
+?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <!-- Meta Tags relacionado a SEO -->
     <meta charset="UTF-8">
@@ -43,8 +45,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="estilos/index.css">
 
+    <!-- CSS - Mapa Leafletjs -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
+
+    <!-- JS - Mapa Leafletjs -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
+
     <title>VersoVivo</title>
 </head>
+
 <body>
 
     <!-- Inicio Cabeçalho -->
@@ -52,6 +62,7 @@
         <!-- Inicio Barra de nav -->
         <?php include("componentes/nav.php") ?>
         <!-- Fim Barra de nav -->
+
 
         <!-- Banner de fundo dos Mc's -->
         <div class="banner">
@@ -74,25 +85,25 @@
 
     <br><br>
 
-    <!--Localização das batalhas-->
-    <div class="tudo">
 
+<div class="tudo">
+    
+        <!--Localização das batalhas-->
         <?php do { ?>
-
-        <div class="loc1">
-            <div class="img1">
-                <img src="imagens/batalha_aldeia.jpg" alt="<?php echo($caixaBatalhas["titulo_eventos"]); ?>" title="<?php echo($caixaBatalhas["titulo_eventos"]); ?>" style="width: 20%; border-radius: 20px;">
-                <p class="titulo1"><b><?php echo ($caixaBatalhas["titulo_eventos"]); ?></b></p>
-                <p class="subtitulo1"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i> <?php echo($caixaBatalhas["endereco"]); ?>, <?php echo($caixaBatalhas["bairro"]); ?></b></p>
-                <p class="data1"><b><?php echo($dataHoraFormatada); ?></b></p>
+    
+            <div class="loc1">
+                <div class="img1">
+                    <img src="imagens/bd_versovivo/<?php echo ($caixaBatalhas['imagem1_thumb']); ?>" alt="<?php echo ($caixaBatalhas["titulo_eventos"]); ?>" title="<?php echo ($caixaBatalhas["titulo_eventos"]); ?>" style="width: 20%; border-radius: 20px;">
+                    <p class="titulo1"><b><?php echo ($caixaBatalhas["titulo_eventos"]); ?></b></p>
+                    <p class="subtitulo1"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i> <?php echo ($caixaBatalhas["endereco"]); ?>, <?php echo ($caixaBatalhas["bairro"]); ?></b></p>
+                    <p class="data1"><b><?php echo ($dataHoraFormatada); ?></b></p>
+                </div>
             </div>
-        </div>
-        <hr class="linha_horizontal">
-        
-        <?php }while($caixaBatalhas = mysqli_fetch_assoc($batalhas)); ?>
-
-    </div>
-    <!--Fim de localização das batalhas-->
+            <hr class="linha_horizontal">
+    
+        <?php } while ($caixaBatalhas = mysqli_fetch_assoc($batalhas)); ?>
+        <!--Fim de localização das batalhas-->
+</div>
 
 
     <br>
@@ -100,18 +111,19 @@
 
     <!--Botão de ver mais-->
     <div class="button1">
-    <button class="dropdown-title" onclick="window.location.href='batalhas.html'" style="border: 2px solid rgb(255, 255, 255); border-radius: 6px; display: flex; align-items: center; width: 110px; margin-left: 63%; height: 40px; background-color: transparent; cursor: pointer;">
-    <b style="margin-left: 10%; white-space: nowrap; color: #ffffff; font-size: 13px;">Ver tudo</b>
-    <i class="fas fa-chevron-down" style="margin-left: 20px; color: #BF4410;"></i>
-    </button>
+        <button class="dropdown-title" onclick="window.location.href='batalhas.php'" style="border: 2px solid rgb(255, 255, 255); border-radius: 6px; display: flex; align-items: center; width: 110px; margin-left: 63%; height: 40px; background-color: transparent; cursor: pointer;">
+            <b style="margin-left: 10%; white-space: nowrap; color: #ffffff; font-size: 13px;">Ver tudo</b>
+            <i class="fas fa-chevron-down" style="margin-left: 20px; color: #BF4410;"></i>
+        </button>
     </div>
-   <!--Fim do botão de ver mais-->
+    <!--Fim do botão de ver mais-->
 
     <br><br><br><br><br><br>
 
     <!--Fundo dos Mc's-->
     <div class="banner">
-        <img src="imagens/fundo_rapper.jpeg" alt="Banner dos MC's" title="Banner dos MC's" style="width: 140%; margin-left: -40%; height: 800px; filter: brightness(50%);"><p class="mais_eventos" style="margin-top: -600px; margin-left: 60px; filter: brightness(100%);">Principais MC's</p>
+        <img src="imagens/fundo_rapper.jpeg" alt="Banner dos MC's" title="Banner dos MC's" style="width: 140%; margin-left: -40%; height: 800px; filter: brightness(50%);">
+        <p class="mais_eventos" style="margin-top: -600px; margin-left: 60px; filter: brightness(100%);">Principais MC's</p>
     </div>
     <!--Fim do fundo dos Mc's-->
 
@@ -122,14 +134,23 @@
     <div class="blocos" style="margin-left: 50%; margin-top: -26%; position: relative; position: absolute;">
         <div class="container">
             <div class="linha" style="margin-left: -187%;">
+
                 <div class="bloco1" style="margin-right: 50px;"><img src="imagens/apolo.jpg" alt="Imagem 1"><p class="nome1">Vulgo</p></div>
+
                 <div class="bloco2" style="margin-right: 50px;"><img src="imagens/apolo.jpg" alt="Imagem 2"><p class="nome2">Vulgo</p></div>
+
                 <div class="bloco3"><img src="imagens/apolo.jpg" alt="Imagem 3"><p class="nome3">Vulgo</p></div>
+
             </div>
+            
             <div class="linha" style="margin-left: -187%;">
+                
                 <div class="bloco4" style="margin-right: 50px;"><img src="imagens/apolo.jpg" alt="Imagem 4"><p class="nome3">Vulgo</p></div>
+
                 <div class="bloco5" style="margin-right: 50px;"><img src="imagens/apolo.jpg" alt="Imagem 5"><p class="nome4">Vulgo</p></div>
+
                 <div class="bloco6"><img src="imagens/apolo.jpg" alt="Imagem 6"><p class="nome6">Vulgo</p></div>
+
             </div>
         </div>
     </div>
@@ -144,20 +165,18 @@
 
     <!--Botão de ver mais-->
     <div class="button2">
-    <button class="dropdown-title" onclick="window.location.href='mc.html'" style="border: 2px solid rgb(255, 255, 255); border-radius: 6px; display: flex; align-items: center; width: 110px; margin-left: 75%; height: 40px; background-color: transparent; cursor: pointer; margin-top: 8%;">
-        <b style="margin-left: 10%; white-space: nowrap; color: #BF4410; font-size: 13px;">Ver tudo</b>
-        <i class="fas fa-chevron-down" style="margin-left: 20px; color: #BF4410;"></i>
-    </button>
+        <button class="dropdown-title" onclick="window.location.href='mc.php'" style="border: 2px solid rgb(255, 255, 255); border-radius: 6px; display: flex; align-items: center; width: 110px; margin-left: 75%; height: 40px; background-color: transparent; cursor: pointer; margin-top: 8%;">
+            <b style="margin-left: 10%; white-space: nowrap; color: #BF4410; font-size: 13px;">Ver tudo</b>
+            <i class="fas fa-chevron-down" style="margin-left: 20px; color: #BF4410;"></i>
+        </button>
     </div>
     <!--Fim do botão de ver mais-->
 
 
     <!-- Inicio Rodapé -->
-    <div class="footer" style="padding: 0; margin: 0; overflow: hidden;">
-        <?php  include("componentes/footer.php") ?>
-    </div>
+     <?php include("componentes/footer.php"); ?>
     <!-- Fim Rodapé -->
-     
+
 
     <!-- jQuery versão 3.6.1 com correção de segurança -->
     <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
@@ -192,12 +211,12 @@
     <!-- Bootstrap versão 5.3.3 com funcionalidades de bundle para versões mais recentes -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Scripts PHP para otimização e desempenho -->
-    <?php 
-        mysqli_free_result($batalhas);
-        mysqli_free_result($mcs);
+    <?php
+    mysqli_free_result($batalhas);
+    mysqli_free_result($mcs);
 
-        mysqli_close($conecta_versovivo);
+    mysqli_close($conecta_versovivo);
     ?>
 </body>
+
 </html>
--- mc.php
+++ mc.php
@@ -1,89 +1,70 @@
+<?php require_once("paistropical.php") ?>
+
+<!-- Script para listar os MCs -->
+<?php
+$listarMcs = "SELECT * FROM usuarios WHERE status_conta = 1";
+
+$mcs = mysqli_query($conecta_versovivo, $listarMcs);
+
+$caixasMcs = mysqli_fetch_assoc($mcs);
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <!-- Meta Tags relacionado a SEO -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Participe do site de batalhas de rima e freestyle, onde você pode assistir às melhores competições de rap ao vivo, acompanhar o calendário de eventos e ficar por dentro das próximas batalhas. Venha fazer parte da cultura hip hop!">
-    <meta name="keywords" content="Batalha de rima, Rap, Freestyle, Cultura hip-hop, MCs, Competição de rap, Rimas criativas, Letras de rap, Eventos de freestyle, Comunidade de rappers, Beatmaking, Hip-hop nacional, Colaborações, História do rap, Vídeos de batalhas, batalha de rima, batalhas de rap, freestyle, MCs, rima improvisada, eventos de rap, batalhas de MCs, batalha de freestyle, batalhas de rima online, batalhas de rima ao vivo, rap improvisado, rimas de improviso, rap ao vivo, competições de rap, rap battles, rimas online, assistir batalhas de rima, próximos eventos de rap, calendário de batalhas, eventos de rima, batalhas de rima 2024, plataforma de rap, assistir rap online, agenda de batalhas de MCs, comunidade de rap, cultura hip hop, criar conta MC, site de batalhas de rima, eventos de hip hop, batalha de rima Brasil, competições de freestyle, próximos shows de rap, rap nacional, MCs brasileiros, rima de improviso ao vivo, batalhas de rima famosas, calendário de rimas, MCs de freestyle, eventos de hip hop online, shows de rap">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="index, follow">
-    <meta name="language" content="pt-BR">
-    <meta name="author" content="Verso Vivo - Batalhas de Rima e Cultura Rap">
-    <meta name="publisher" content="Verso Vivo - Batalhas de Rima e Cultura Rap">
-    <!-- Fim Meta Tags relacionado a SEO -->
+  <!-- Meta Tags relacionado a SEO -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Participe do site de batalhas de rima e freestyle, onde você pode assistir às melhores competições de rap ao vivo, acompanhar o calendário de eventos e ficar por dentro das próximas batalhas. Venha fazer parte da cultura hip hop!">
+  <meta name="keywords" content="Batalha de rima, Rap, Freestyle, Cultura hip-hop, MCs, Competição de rap, Rimas criativas, Letras de rap, Eventos de freestyle, Comunidade de rappers, Beatmaking, Hip-hop nacional, Colaborações, História do rap, Vídeos de batalhas, batalha de rima, batalhas de rap, freestyle, MCs, rima improvisada, eventos de rap, batalhas de MCs, batalha de freestyle, batalhas de rima online, batalhas de rima ao vivo, rap improvisado, rimas de improviso, rap ao vivo, competições de rap, rap battles, rimas online, assistir batalhas de rima, próximos eventos de rap, calendário de batalhas, eventos de rima, batalhas de rima 2024, plataforma de rap, assistir rap online, agenda de batalhas de MCs, comunidade de rap, cultura hip hop, criar conta MC, site de batalhas de rima, eventos de hip hop, batalha de rima Brasil, competições de freestyle, próximos shows de rap, rap nacional, MCs brasileiros, rima de improviso ao vivo, batalhas de rima famosas, calendário de rimas, MCs de freestyle, eventos de hip hop online, shows de rap">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="robots" content="index, follow">
+  <meta name="language" content="pt-BR">
+  <meta name="author" content="Verso Vivo - Batalhas de Rima e Cultura Rap">
+  <meta name="publisher" content="Verso Vivo - Batalhas de Rima e Cultura Rap">
+  <!-- Fim Meta Tags relacionado a SEO -->
 
   <link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="estilos/mc.css">
-  
+
   <title>Página dos Mc's</title>
 </head>
+
 <body>
 
   <!-- Começo do cabeçalho -->
   <header>
     <!-- Inicio da Barra de Nav -->
-     <?php include("componentes/nav.php") ?>
+    <?php include("componentes/nav.php") ?>
     <!-- Fim da Barra de Nav -->
   </header>
   <!-- Fim do cabeçalho -->
 
   <!-- Todos os mc's -->
   <p class="nome">Todos os Mc's</p>
+
   <div class="tudo">
-    <div class="loc1">
-      <img src="imagens/apolo.jpg" alt="img1" title="imagem1" style="width: 16%; border-radius: 20px;">
-      <p class="titulo1"><b>Apolo</b></p>
-      <p class="subtitulo1"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data1"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
 
-    <div class="loc2">
-      <img src="imagens/apolo.jpg" alt="img2" title="imagem2" style="width: 16%; border-radius: 20px;">
-      <p class="titulo2"><b>Apolo</b></p>
-      <p class="subtitulo2"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data2"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
+    <?php do { ?>
 
-    <div class="loc3">
-      <img src="imagens/apolo.jpg" alt="img3" title="imagem3" style="width: 16%; border-radius: 20px;">
-      <p class="titulo3"><b>Apolo</b></p>
-      <p class="subtitulo3"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data3"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
+      <div class="loc1">
+        <img src="imagens/apolo.jpg" alt="<?php echo ($caixasMcs["nome_guerra"]); ?>" title="<?php echo ($caixasMcs["nome_guerra"]); ?>" style="width: 16%; border-radius: 20px;">
+        <p class="titulo1"><b><?php echo ($caixasMcs["descricao"]); ?></b></p>
+        <p class="subtitulo1"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i><?php echo ($caixasMcs["email"]); ?></b></p>
+        <p class="data1"><b><?php echo ($caixasMcs["nome_guerra"]); ?></b></p>
+      </div>
+      <hr class="linha_horizontal">
 
-    <div class="loc4">
-      <img src="imagens/apolo.jpg" alt="img4" title="imagem4" style="width: 16%; border-radius: 20px;">
-      <p class="titulo4"><b>Apolo</b></p>
-      <p class="subtitulo4"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data4"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
+    <?php } while ($caixasMcs = mysqli_fetch_assoc($mcs)); ?>
 
-    <div class="loc5">
-      <img src="imagens/apolo.jpg" alt="img5" title="imagem5" style="width: 16%; border-radius: 20px;">
-      <p class="titulo5"><b>Apolo</b></p>
-      <p class="subtitulo5"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data5"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
-
-    <div class="loc6">
-      <img src="imagens/apolo.jpg" alt="img6" title="imagem6" style="width: 16%; border-radius: 20px;">
-      <p class="titulo6"><b>Apolo</b></p>
-      <p class="subtitulo6"><b><i class="fas fa-map-marker-alt" style="color: #BF4410;"></i>apolomc@gmail.com</b></p>
-      <p class="data6"><b>Vulgo</b></p>
-    </div>
-    <hr class="linha_horizontal">
   </div>
   <!-- Fim de todos os mc's -->
 
   <!-- Footer -->
-   <?php include("componentes/footer.php") ?>
+  <?php include("componentes/footer.php") ?>
   <!-- Fim do footer -->
 
   <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
@@ -100,6 +81,7 @@
       });
     });
   </script>
-   <!-- Fim do funcionamento do menu burguer -->
+  <!-- Fim do funcionamento do menu burguer -->
 </body>
+
 </html>
